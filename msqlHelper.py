b8f7e20f9a4da57fe6a4cb7340a5f493d67c0c8d
wangkaixuan <2654189525@qq.com>
2018-02-25 09:36:15 -0800

    实现了对MySQL增删改查操作的封装 1. 学习某项技能的时候,最靠谱的教程就是其官网网站的document 2. MySQL每次查询完后关闭的是cursor的连接,而不是数据库的连接,数据库的连接要在,析构函数中关闭

---
+++ msqlHelper.py
@@ -0,0 +1,59 @@
+from mysql import connector
+
+class MysqlHelper():
+    def __init__(self,user,password):
+        self.conn = connector.connect(user = user,
+                                      password = password,
+                                      host = '127.0.0.1',
+                                      database = 'ex_redis'
+                                      )
+        cursor = self.conn.cursor()
+        try:
+            cursor.execute('create table user(name varchar(20) primary key,password varchar(40))')
+        except Exception as e:
+            print(e.args)
+        # cursor.execute('insert into table user(name,password) values(%s,%s)',[test,password])
+        self.conn.commit()
+        cursor.close()
+    def __del__(self):
+        self.conn.close()
+
+    def __cud(self,sql,params):
+        cs1 =self.conn.cursor()
+        rows = cs1.execute(sql,params)
+        self.conn.commit()
+        cs1.close()
+        self.conn.close()
+        return rows
+
+    def insert(self,sql,params):
+        return self.__cud(sql,params)
+
+    def update(self,sql,params):
+        return self.__cud(sql,params)
+
+    def delete(self,sql,params):
+        return self.__cud(sql,params)
+
+    def fetchone(self,sql,params=[]):
+        csl = self.conn.cursor()
+        csl.execute(sql,params)
+        row = csl.fetchone()
+        csl.close()
+        self.conn.close()
+        return row
+    def fetchall(self,sql,params):
+        csl = self.conn.cursor()
+        csl.execute(sql,params)
+        rows = csl.fetchall()
+        csl.close()
+        self.conn.close()
+        return rows
+
+if __name__ == '__main__':
+    m = MysqlHelper('root','iroanMYS47')
+    import hashlib
+    sha1 = hashlib.sha1()
+    sha1.update('iroan'.encode())
+    pwd = sha1.hexdigest()
+    m.insert('insert into user(name,password) values(%s,%s)',['iroan',pwd])
