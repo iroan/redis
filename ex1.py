2c6f92135352716a32853dc8b785c962356c0930
wangkaixuan <2654189525@qq.com>
2018-02-25 06:49:16 -0800

    Redis中不能重复插入不同类型但是相同名字的key的数据

---
+++ ex1.py
@@ -0,0 +1,31 @@
+import redis
+
+try:
+    r = redis.StrictRedis(host='localhost',port=6379)
+except Exception as e:
+    print(e.args)
+
+def ex_string():
+    # 练习string
+    r.set('name', 'wangkaixuan')
+    print(r.get('name').decode())
+
+    r.set('height', 175)
+    r.incr('height')
+    r.incr('height')
+    r.incr('height')
+    print(r.get('height').decode())
+
+    r.set('inputing', 'input...', ex=2)
+    print(r.get('inputing').decode())
+    print(r.ttl('inputing'))
+    import time
+    time.sleep(2)
+    print(r.get('inputing').decode())
+
+
+r.lpush('name1','wangkaixuan')
+r.lpush('name1','wangkaijun')
+r.lpush('name1','chengangrong')
+r.rpush('name1','longhao')
+print(r.lrange('name1',0,-1))
